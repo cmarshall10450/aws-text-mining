b6bd6d9d95647d9e329a4312a36329d4af2c24a8
cmarshall10450 <cmarshall10450@gmail.com>
2018-03-19 16:15:54 +0000

    Added program paramaters

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import json
-import re
 import sys
+import re
+import argparse
 from utils import analyse
 from aws import list_objects, get_object, put_object
 
-bucket = sys.argv[1]
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--bucket', help='S3 bucket')
+parser.add_argument('--inputPrefix', help='Input prefix')
+parser.add_argument('--outputPrefix', help='Output prefix')
+args = vars(parser.parse_args())
+
+bucket = args['bucket']
 
 objects = list_objects(
-        bucket=bucket,
-        prefix='inbound/text-analysis/',
-        delimiter='/'
-        )['Contents']
+    bucket=bucket,
+    prefix=args['inputPrefix'],
+    delimiter='/'
+)['Contents']
 
 regex = re.compile(r'\/$')
 objects = list(filter(lambda x: not regex.search(x['Key']), objects))
@@ -18,18 +26,19 @@ objects = list(filter(lambda x: not regex.search(x['Key']), objects))
 for item in objects:
     key = item['Key']
     item_data = get_object(
-            bucket=bucket,
-            key=key
-            )
+        bucket=bucket,
+        key=key
+    )
 
     text = item_data['Body'].read()
     data = analyse(text)
 
     parts = key.split('/')
-    newKey = 'sandbox/' + parts[len(parts) -1 ].split('.')[0] + '.json'
+    newKey = args['outputPrefix'] + \
+        parts[len(parts) - 1].split('.')[0] + '.json'
     print(newKey)
     put_object(
-            bucket=bucket,
-            key=newKey,
-            body=data
-            )
+        bucket=bucket,
+        key=newKey,
+        body=data
+    )
