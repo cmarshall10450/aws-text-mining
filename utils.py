09863ae6b888642851c117be6d11e3e333c82d0c
cmarshall10450 <cmarshall10450@gmail.com>
2018-03-19 17:02:08 +0000

    Added entity count and key phrase count

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 from aws import *
 
+
 def flatten(array):
     return [item for sublist in array for item in sublist]
 
+
 def chunks(l, n):
     return [l[i:i + n] for i in range(0, len(l), n)]
+
 
 def analyse(text):
     chunk_size = 5000
@@ -14,19 +17,32 @@ def analyse(text):
     result = chunks(text_chunks, max_result_length)
 
     sentiments = [get_batch_sentiment(text_list=result[i])['ResultList']
-                    for i in range(0, len(result))]
+                  for i in range(0, len(result))]
     sentiments = flatten(sentiments)
 
     entities = [get_batch_entities(text_list=result[i])['ResultList']
-                    for i in range(0, len(result))]
+                for i in range(0, len(result))]
     entities = flatten(entities)
     entities = flatten([item['Entities'] for item in entities])
 
+    entity_count = {}
+    for entity in entities:
+        if entity['Text'] in entity_count.keys():
+            entity_count[entity['Text']] += 1
+        else:
+            entity_count[entity['Text']] = 1
+
     key_phrases = [get_batch_key_phrases(text_list=result[i])['ResultList']
-               for i in range(0, len(result))]
+                   for i in range(0, len(result))]
     key_phrases = flatten(key_phrases)
     key_phrases = flatten([item['KeyPhrases'] for item in key_phrases])
 
+    key_phrase_count = {}
+    for key_phrase in key_phrases:
+        if key_phrase['Text'] in key_phrase_count.keys():
+            key_phrase_count[key_phrase['Text']] += 1
+        else:
+            key_phrase_count[key_phrase['Text']] = 1
 
     for i in range(0, len(sentiments)):
         sentiments[i]['Index'] = i
@@ -34,10 +50,11 @@ def analyse(text):
     data = {}
 
     data['OverallSentimentScores'], data['OverallSentiment'] = get_overall_sentiment(
-                                                                sentiments)
+        sentiments)
     data['Sentiments'] = sentiments
     data['Entities'] = entities
+    data['EntityCount'] = entity_count
     data['KeyPhrases'] = key_phrases
+    data['KeyPhraseCount'] = key_phrase_count
 
     return data
-
